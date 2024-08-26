3791d42b21e062395af3644cd98fb541c826adff
pablo_l <pablo93bio@gmail.com>
2024-08-26 10:29:39 +0200

    Optimize MainActivity.kt

--- app/src/main/java/com/pavdev/autocommit/MainActivity.kt
+++ app/src/main/java/com/pavdev/autocommit/MainActivity.kt
@@ -1,25 +1,10 @@
 package com.pavdev.autocommit
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.pavdev.autocommit.ui.screens.MainScreen
 import com.pavdev.autocommit.ui.theme.AutocommitTheme
 import com.pavdev.autocommit.ui.viewmodels.MainViewModel
