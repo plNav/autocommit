3321ee310252f56ed1da1577ec6778e4617de004
pablo_l <pablo93bio@gmail.com>
2024-08-29 16:17:59 +0200

    Old Settings Deleted

--- app/src/main/java/com/pavdev/autocommit/MainActivity.kt
+++ app/src/main/java/com/pavdev/autocommit/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.pavdev.autocommit.ui.screens.ConfigScreen
 import com.pavdev.autocommit.ui.screens.MainScreen
 import com.pavdev.autocommit.ui.screens.SettingsScreen
 import com.pavdev.autocommit.ui.theme.AutocommitTheme
