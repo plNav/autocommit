89f2a66fdc7ee5f2f6b1a8571a365658e9ba62a1
pablo_l <pablo93bio@gmail.com>
2024-09-15 17:57:43 +0200

    Test with Ale

--- app/src/main/java/com/pavdev/autocommit/MainActivity.kt
+++ app/src/main/java/com/pavdev/autocommit/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             AutocommitTheme {
                 val navController = rememberNavController()
                 SetupNavGraph(navController = navController, mainViewModel = mainViewModel)
